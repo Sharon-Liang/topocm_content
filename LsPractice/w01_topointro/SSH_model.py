603a6ff8f08b0e92c3b90da9c1b5b58a6a03088a
Sharon-Liang <liangshuang228@gmail.com>
2019-10-24 12:46:17 -0700

    The begining of task 2 !

---
+++ LsPractice/w01_topointro/SSH_model.py
@@ -0,0 +1,8 @@
+#------This is the 2nd task of week-1 assignment-------------------------------
+import sys
+sys.path.append('./code')  #import modules in folder : code
+import pfaffian as pf
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
